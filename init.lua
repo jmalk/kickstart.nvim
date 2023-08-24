8bcfdc13375ed91775ba0c8fe21259781671c4b8
Josh Malkinson <joshua.malkinson@coop.co.uk>
2023-08-24 08:29:00 +0100

    Uncomment some language servers

--- init.lua
+++ init.lua
@@ -438,9 +438,9 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
